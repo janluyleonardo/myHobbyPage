1dfc62b843e8dd4b34949c63979b5b0395814943
janluyleonardo <janluy.moreno@gmail.com>
2025-09-25 11:26:50 -0500

    ajustes varios de consolidacion del proyecto

--- script.js
+++ script.js
@@ -34,3 +34,26 @@ async function cargarComentarios() {
 
 // Cargar comentarios al inicio
 cargarComentarios();
+
+// Espera a que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', function() {
+  const footerText = "Proyecto demo - Fútbol Sala - By MorJan";
+  const footerElement = document.getElementById('footer-typing');
+  let i = 0;
+
+  // Función para el efecto de escritura
+  function typeWriter() {
+    if (i < footerText.length) {
+      footerElement.textContent += footerText.charAt(i);
+      i++;
+      setTimeout(typeWriter, 250); // Velocidad de escritura (100ms por carácter)
+    } else {
+      // Cuando termina de escribir, agrega el cursor parpadeante
+      footerElement.style.borderRight = '0.15em solid orange';
+    }
+  }
+
+  // Inicia el efecto al cargar la página
+  footerElement.textContent = ''; // Limpia el contenido inicial
+  typeWriter();
+});
